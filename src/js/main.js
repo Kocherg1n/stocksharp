793528e531e3425fd846160d00fa45b718f0773e
Yuri Kochergin <kocherg1n@icloud.com>
2020-04-22 04:22:26 +0300

    single page static layout ready

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+// import $ from '../local_modules/jquery/dist/jquery.min'
+//
+// $(document).ready(() => {
+//     // eslint-disable-next-line no-console
+//     console.log(`document ready`)
+// });
+//
+//
